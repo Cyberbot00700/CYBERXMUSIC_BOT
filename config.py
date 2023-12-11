5e3460c8776107553ce021ca63208832df927ebc
Cyber bots <149052582+Cyberbot00700@users.noreply.github.com>
2023-12-12 04:08:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "",
+    "https://github.com/Cyberbot00700/CYBERXMUSIC_BOT.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
